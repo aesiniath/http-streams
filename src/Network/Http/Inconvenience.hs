c6776eab89e92e8534e84159f76a25565d10052f
Andrew Cowie <andrew@operationaldynamics.com>
2012-12-29 11:42:49 +1100

    Implenet put convenience method
    Implement the put convenience API method, copying the implmentation
    from the original post function.
    
    Expand explanitory note about content needing to fit in memory and add
    an example of using the underlying API manually.

--- src/Network/Http/Inconvenience.hs
+++ src/Network/Http/Inconvenience.hs
@@ -80,13 +80,13 @@ get u handler = bracket
         _ <- handler p b
         return ()
 
--- | TODO
-{-
-    Hm. RFC 2616 requires that we send a Content-Length header, but we
-    we can't figure that out unless we've run through the outbound
-    stream, which means running entirely into memory. Bummer, but ok,
-    fine, don't use this for large file uploads.
--}
+--
+-- | Send content to a server via an HTTP POST request. Use this
+-- function if you have an 'OutputStream' with the body content.
+-- See the note in 'put' about @Content-Length@ and the request
+-- body needing to fit into memory. If that is inconvenient, just use
+-- the underlying "Network.Http.Client" API directly.
+--
 post :: URI
     -> ContentType
     -> (OutputStream ByteString -> IO α)
@@ -136,8 +136,67 @@ runBody body = do
     return (inputStreamBody i3, fromIntegral n)
 
 
+--
+-- | Place content on the server at the given URL via an HTTP PUT
+-- request, specifying the content type and a function to write the
+-- content to the supplied 'OutputStream'. You might see:
+-- 
+-- > put "http://s3.example.com/bucket42/object149" "text/plain" (fileBody "hello.txt") (\p i -> do
+-- >     putStr $ show p
+-- >     Streams.connect i stdout)
+-- 
+-- RFC 2616 requires that we send a @Content-Length@ header, but we
+-- can't figure that out unless we've run through the outbound stream,
+-- which means the entity body being sent must fit entirely into memory.
+-- If you need to send something large and already know the size, use
+-- the underlying API directly and you can actually stream the body
+-- instead. For example:
+-- 
+-- > n <- getSize "hello.txt"
+-- > c <- openConnection "s3.example.com" 80
+-- > q <- buildRequest c $ do
+-- >     http PUT "/bucket42/object149"
+-- >     setContentType "text/plain"
+-- >     setContentLength n
+-- > p <- sendRequest c q (fileBody "hello.txt")
+-- >
+-- > closeConnection c
+-- > assert (getStatusCode p == 201)
+-- 
+-- or something to that effect; the key being that you can set the
+-- @Content-Length@ header correctly, and then stream the content using
+-- (in this example) 'fileBody'.
+--
+put :: URI
+    -> ContentType
+    -> (OutputStream ByteString -> IO α)
+    -> (Response -> InputStream ByteString -> IO α)
+    -> IO ()
+put u t body handler = bracket
+    (establish u)
+    (teardown)
+    (process)
+  where
+    teardown = closeConnection
 
--- | TODO
-put  :: URI -> (OutputStream ByteString -> IO α) -> (Response -> InputStream ByteString -> IO α) -> IO ()
-put = undefined
+    path = concat [uriPath u, uriQuery u, uriFragment u]
+
+    process :: Connection -> IO ()
+    process c = do
+        (e,n) <- runBody body
+        
+        let len = S.pack $ show (n :: Int)
+
+        q <- buildRequest c $ do
+            http PUT path
+            setAccept "*/*"
+            setHeader "Content-Type" t
+            setHeader "Content-Length" len
+
+        p <- sendRequest c q e
+
+        b <- receiveResponse c p
+        
+        _ <- handler p b
+        return ()
 
