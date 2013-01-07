5df0ffbbd5180a00083ddce363a72e9828ba9196
Andrew Cowie <andrew@operationaldynamics.com>
2013-01-07 18:50:37 +1100

    Use bytestring-lexing for Int parsing
    Performance improvement to receiveResponse: based on profiline, switch
    to using Data.ByteString.Lex.Integral's readDecimal functions to parse
    the Content-Length header into an Int. Thanks to Carl Howells for his
    suggestion of bytestring-lexing.

--- src/Network/Http/Connection.hs
+++ src/Network/Http/Connection.hs
@@ -36,6 +36,7 @@ import qualified Data.ByteString.Char8 as S
 import Data.CaseInsensitive (mk)
 import Control.Exception (Exception, throwIO)
 import Data.Typeable (Typeable)
+import Data.ByteString.Lex.Integral (readDecimal_)
 import Network.Http.Types
 import Network.Http.ResponseParser
 
@@ -211,7 +212,7 @@ receiveResponse c p = do
     header = getHeader p
     
     n = case header "Content-Length" of
-        Just x' -> read $ S.unpack x' :: Int
+        Just x' -> readDecimal_ x' :: Int
         Nothing -> 0
 
 
