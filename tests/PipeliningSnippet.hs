a8b96cefe46961b07bdd631335f3d598f610fc17
Andrew Cowie <andrew@operationaldynamics.com>
2013-01-21 12:08:10 +1100

    Fork snippet to PipeliningSnippet

---
+++ tests/PipeliningSnippet.hs
@@ -0,0 +1,62 @@
+--
+-- HTTP client for use with io-streams
+--
+-- Copyright © 2012-2013 Operational Dynamics Consulting, Pty Ltd
+--
+-- The code in this file, and the program it is a part of, is made
+-- available to you by its authors as open source software: you can
+-- redistribute it and/or modify it under a BSD licence.
+--
+
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS -fno-warn-unused-imports #-}
+
+module Snippet where
+
+import Control.Exception (bracket)
+import Network.Http.Client
+
+--
+-- Otherwise redundent imports, but useful for testing in GHCi.
+--
+
+import Blaze.ByteString.Builder (Builder)
+import qualified Blaze.ByteString.Builder as Builder
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as S
+import Debug.Trace
+import System.Exit (exitSuccess)
+import System.IO.Streams (InputStream, OutputStream, stdout)
+import qualified System.IO.Streams as Streams
+
+
+main :: IO ()
+main = do
+    c <- openConnection "kernel.operationaldynamics.com" 58080
+    putStrLn $ show c
+
+    q1 <- buildRequest c $ do
+        http GET "/time"
+        setAccept "text/plain"
+
+    q2 <- buildRequest c $ do
+        http GET "/time"
+        setAccept "text/plain"
+
+    sendRequest c q1 emptyBody
+    sendRequest c q2 emptyBody
+
+    receiveResponse c debugHandler
+    receiveResponse c debugHandler
+
+    closeConnection c
+
+
+printHandler :: Response -> InputStream ByteString -> IO ()
+printHandler _ i = do
+    Streams.connect i stdout
+
+debugHandler :: Response -> InputStream ByteString -> IO ()
+debugHandler p i = do
+    putStr $ show p
+    Streams.connect i stdout
