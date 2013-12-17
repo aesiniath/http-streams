c189a95f3aaa8d6b2ad7856e61962946e8e078db
Andrew Cowie <andrew@operationaldynamics.com>
2013-12-17 21:38:06 +1100

    Expose setTransferEncoding
    Fix test suite by exposing setTransferEncoding function, complement of
    setContentLength, inherited from **http-common**.
    
    Bump API version to 0.7.1

--- tests/TestSuite.hs
+++ tests/TestSuite.hs
@@ -66,7 +66,7 @@ import Network.Http.Client
 import Network.Http.Connection (Connection (..))
 import Network.Http.Inconvenience (HttpClientError (..),
                                    TooManyRedirects (..), splitURI)
-import Network.Http.Internal (Request (..), Response (..), EntityBody(..),
+import Network.Http.Internal (Request (..), Response (..),
                               composeRequestBytes, lookupHeader)
 import Network.Http.ResponseParser (readDecimal, readResponseHeader)
 
@@ -469,11 +469,10 @@ testSendBodyFor meth =
     it ("Sends a request body for " ++ show meth) $ do
         c <- openConnection "localhost" localPort
 
-        q' <- buildRequest $ do
+        q <- buildRequest $ do
             http meth "/size"
             setContentType "text/plain"
-            setHeader "Transfer-Encoding" "chunked"
-        let q = q'{qBody = Chunking}
+            setTransferEncoding
 
         sendRequest c q (\o -> do
             Streams.write (Just (Builder.fromString "a request")) o)
